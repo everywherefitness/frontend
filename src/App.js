3c31ca0f8a877a8c78d23342282f5c5a11cdea96
Stu Darsey <77475990+stu-wd@users.noreply.github.com>
2021-11-16 14:34:49 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Register from './components/Register';
+// import React, { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <Register />
+      
+    </div>
+  );
+}
+
+export default App;
